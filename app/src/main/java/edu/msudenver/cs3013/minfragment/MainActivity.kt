2e58c18e0e9fc6e2c1387cc26eae16a579e2a5c3
t-radke <tradke@msudenver.edu>
2025-04-05 11:14:27 -0600

    assignment functionality working

--- app/src/main/java/edu/msudenver/cs3013/minfragment/MainActivity.kt
+++ app/src/main/java/edu/msudenver/cs3013/minfragment/MainActivity.kt
@@ -1,20 +1,32 @@
+// Tyler Radke
+// MinFragment Assignment
+// MainActivity.kt file
+
 package edu.msudenver.cs3013.minfragment
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentContainerView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+    }
+
+    //Start Fragment Function
+
+    fun startFragment(view: View) {
+        findViewById<FragmentContainerView>(R.id.fragment_container)?.let { frameLayout ->
+            val blueFragment = BlueFragment.newInstance("FIRST")
+            supportFragmentManager.beginTransaction().add(frameLayout.id, blueFragment).addToBackStack(null).commit()
         }
     }
+
 }
